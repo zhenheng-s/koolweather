c664e5cfa5a42a610a02b33d0b773ca3656a9d36
zhenheng_s <zhenheng_s@163.com>
2017-06-29 16:30:50 +0800

    three

---
+++ app/src/main/java/com/szh/koolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.szh.koolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by szh on 2017/6/29.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private int cityCode;
+    private String cityName;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/szh/koolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.szh.koolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by szh on 2017/6/29.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
